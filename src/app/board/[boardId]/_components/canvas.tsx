4a305bdd715641750f6340b6d8553c20b5e1c732
Kim-777 <iloo3012@naver.com>
2024-02-11 22:33:31 +0900

    create selection-box component

--- src/app/board/[boardId]/_components/canvas.tsx
+++ src/app/board/[boardId]/_components/canvas.tsx
@@ -1,18 +1,26 @@
 'use client';
 
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { CanvasMode } from '@/types/canvas';
-import type { Camera, CanvasState, Color, Point, LayerType } from '@/types/canvas';
+import type { Camera, CanvasState, Color, Point, LayerType, Side, XYWH } from '@/types/canvas';
 
 import { Info } from './info';
 import { Participants } from './participants';
 import { Toolbar } from './toolbar';
-import { useHistory, useCanRedo, useCanUndo, useMutation, useStorage } from '../../../../../liveblocks.config';
+import {
+  useHistory,
+  useCanRedo,
+  useCanUndo,
+  useMutation,
+  useStorage,
+  useOthersMapped,
+} from '../../../../../liveblocks.config';
 import { CursorsPresence } from './cursors-presence';
-import { pointerEventToCanvasPoint } from '@/lib/utils';
+import { connectionIdToColor, pointerEventToCanvasPoint } from '@/lib/utils';
 import { nanoid } from 'nanoid';
 import { LiveObject } from '@liveblocks/client';
 import { LayerPreview } from './layer-preview';
+import { SelectionBox } from './selection-box';
 
 const MAX_LAYERS = 100;
 
@@ -27,9 +35,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
   const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
   const [lastUsedColor, setLastUsedColor] = useState<Color>({
-    r: 0,
-    g: 0,
-    b: 0,
+    r: 255,
+    g: 255,
+    b: 255,
   });
 
   const history = useHistory();
@@ -68,6 +76,18 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     [lastUsedColor],
   );
 
+  const onResizeHandlePointerDown = useCallback(
+    (corner: Side, initialBounds: XYWH) => {
+      history.pause();
+      setCanvasState({
+        mode: CanvasMode.Resizing,
+        initialBounds,
+        corner,
+      });
+    },
+    [history],
+  );
+
   const onWheel = useCallback((e: React.WheelEvent) => {
     setCamera((camera) => ({
       x: camera.x - e.deltaX,
@@ -104,6 +124,42 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     [camera, canvasState, history, insertLayer],
   );
 
+  const selections = useOthersMapped((other) => other.presence.selection);
+
+  const onLayerPointerDown = useMutation(
+    ({ self, setMyPresence }, e: React.PointerEvent, layerId: string) => {
+      if (canvasState.mode === CanvasMode.Pencil || canvasState.mode === CanvasMode.Inserting) {
+        return;
+      }
+
+      history.pause();
+      e.stopPropagation();
+
+      const point = pointerEventToCanvasPoint(e, camera);
+
+      if (!self.presence.selection.includes(layerId)) {
+        setMyPresence({ selection: [layerId] }, { addToHistory: true });
+      }
+
+      setCanvasState({ mode: CanvasMode.Translating, current: point });
+    },
+    [setCanvasState, camera, history, canvasState.mode],
+  );
+
+  const layerIdsToColorSelection = useMemo(() => {
+    const newLayerIdsToColorSelection: Record<string, string> = {};
+
+    for (const user of selections) {
+      const [connectionId, selection] = user;
+
+      for (const layerId of selection) {
+        newLayerIdsToColorSelection[layerId] = connectionIdToColor(connectionId);
+      }
+    }
+
+    return newLayerIdsToColorSelection;
+  }, [selections]);
+
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
       <Info boardId={boardId} />
@@ -128,12 +184,11 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             <LayerPreview
               key={layerId}
               id={layerId}
-              onLayerPointerDown={() => {
-                return;
-              }}
-              selectionColor={'#000'}
+              onLayerPointerDown={onLayerPointerDown}
+              selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox onResizeHandlePointerDown={onResizeHandlePointerDown} />
           <CursorsPresence />
         </g>
       </svg>
